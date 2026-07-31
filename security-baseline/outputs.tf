40f30b84120724f63fd77382c7d78c1ea2a6a869
Nabil Islam <nabil.islam@fimatix.com>
2026-07-31 11:22:50 +0100

    feat: add monitoring and CloudTrail log group output

--- security-baseline/outputs.tf
+++ security-baseline/outputs.tf
@@ -7,3 +7,8 @@ output "access_analyzer_arn" {
   description = "The ARN of the IAM Access Analyzer."
   value       = aws_accessanalyzer_analyzer.this.arn
 }
+
+output "cloudtrail_log_group_name" {
+  description = "The name of the CloudWatch Log Group used by CloudTrail."
+  value       = aws_cloudwatch_log_group.cloudtrail.name
+}
