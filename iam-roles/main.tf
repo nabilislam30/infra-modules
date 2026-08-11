db209650201826669a88233f7528d6fbd81f4fe8
Nabil Islam <nabil.islam@fimatix.com>
2026-08-11 18:13:25 +0100

    fix: complete VPC read permissions for OIDC plan

--- iam-roles/main.tf
+++ iam-roles/main.tf
@@ -409,25 +409,28 @@ data "aws_iam_policy_document" "vpc_deployment_permissions" {
       "ec2:DeleteVpc",
       "ec2:DescribeVpcs",
       "ec2:DescribeVpcAttribute",
-      "ec2:DescribeAddressesAttribute",
-      "ec2:DescribeAddresses",
-      "ec2:DescribeAddressesAttribute",
       "ec2:ModifyVpcAttribute",
+
       "ec2:CreateSubnet",
       "ec2:DeleteSubnet",
       "ec2:DescribeSubnets",
       "ec2:ModifySubnetAttribute",
+
       "ec2:CreateInternetGateway",
       "ec2:DeleteInternetGateway",
       "ec2:AttachInternetGateway",
       "ec2:DetachInternetGateway",
       "ec2:DescribeInternetGateways",
+
       "ec2:AllocateAddress",
       "ec2:ReleaseAddress",
       "ec2:DescribeAddresses",
+      "ec2:DescribeAddressesAttribute",
+
       "ec2:CreateNatGateway",
       "ec2:DeleteNatGateway",
       "ec2:DescribeNatGateways",
+
       "ec2:CreateRouteTable",
       "ec2:DeleteRouteTable",
       "ec2:DescribeRouteTables",
@@ -437,6 +440,7 @@ data "aws_iam_policy_document" "vpc_deployment_permissions" {
       "ec2:DeleteRoute",
       "ec2:ReplaceRoute",
       "ec2:ReplaceRouteTableAssociation",
+
       "ec2:CreateSecurityGroup",
       "ec2:DeleteSecurityGroup",
       "ec2:DescribeSecurityGroups",
@@ -444,16 +448,21 @@ data "aws_iam_policy_document" "vpc_deployment_permissions" {
       "ec2:AuthorizeSecurityGroupEgress",
       "ec2:RevokeSecurityGroupIngress",
       "ec2:RevokeSecurityGroupEgress",
+
       "ec2:CreateVpcEndpoint",
       "ec2:DeleteVpcEndpoints",
       "ec2:DescribeVpcEndpoints",
       "ec2:ModifyVpcEndpoint",
       "ec2:DescribeVpcEndpointServices",
+      "ec2:DescribePrefixLists",
+
       "ec2:CreateTags",
       "ec2:DeleteTags",
       "ec2:DescribeTags",
+
       "ec2:DescribeAvailabilityZones",
       "ec2:DescribeNetworkInterfaces",
+
       "ec2:CreateFlowLogs",
       "ec2:DescribeFlowLogs",
       "ec2:DeleteFlowLogs"
